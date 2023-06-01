68a0d94e9fad7634560c9bb989e6926e916f8c43
lalit <lalit.trigunayat@napses.com>
2023-06-01 19:19:54 +0530

    variable changes done

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args){
+        // int, float, char, String, long, double, boolean
+        int marks=18;
+        float averageMarks=12.23f;
+        char grade='A';
+        String name="lalit";
+        boolean isValid=false;
+        double bigNumber=1222222222222222l;
+        System.out.println(marks);
+        System.out.println(averageMarks);
+        System.out.println(grade);
+        System.out.println(name);
+        System.out.println(isValid);
+        System.out.println(bigNumber);
+    }
+}
